a1fca33662e6384d656d34c1888defdbce9338e7
Khansa Khalida Kennedy <khansakennedy11@gmail.com>
2023-03-14 23:00:03 +0700

    Khansakennedy_I0322067

---
+++ Exercise-01.py
@@ -0,0 +1,5 @@
+print ("Khansa Khalida Kennedy")
+print ("I0322067")
+print ("Saya tinggal di Sleman, Provinsi Yogyakarta")
+print ("Tempat tinggal saya salah satu provinsi yang dikenal sebagai Kota wisata, tetapi UMR atau upah minimum yang dihasilkan kota yogyakarta paling rendah dari seluruh provinsi di Jawa. Yogyakarta juga terkenal sebagai kota pelajar karena banyak sekali sekolah sampai universitas yang ada di sini")
+print ("setelah saya lulus dari Teknik Industri UNS, saya berharap bisa menjadi orang yang bermanfaat bagi sekitar saya dan bisa mencapai apa yang saya cita-citakan")
